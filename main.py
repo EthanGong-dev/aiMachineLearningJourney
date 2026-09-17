c6aced3f5978e807d7519f9699678f1ee1d5e2d5
frost <armanarfan481@gmail.com>
2026-09-17 23:03:58 +0800

    part 2 fastAPI

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+import ollama
 
 app = FastAPI()
 
 
-class Question(BaseModel):
+class QuestionRequest(BaseModel):
+    question: str = Field(
+        min_length=1,
+        description="Question that the user wants to ask the AI"
+    )
+
+    temperature: float = Field(
+        default=0.7,
+        ge=0.0,
+        le=1.0,
+        description="Controls how creative the AI response is"
+    )
+
+
+class AIResponse(BaseModel):
     question: str
+    answer: str
 
 
 @app.get("/")
@@ -15,9 +31,23 @@ def home():
     }
 
 
-@app.post("/ask")
-def ask_question(data: Question):
+@app.post("/ask", response_model=AIResponse)
+def ask_question(data: QuestionRequest):
+
+    response = ollama.chat(
+        model="qwen3:1.7b",
+        messages=[
+            {
+                "role": "user",
+                "content": data.question
+            }
+        ],
+        options={
+            "temperature": data.temperature
+        }
+    )
+
     return {
         "question": data.question,
-        "answer": "This is a test AI response."
+        "answer": response["message"]["content"]
     }
